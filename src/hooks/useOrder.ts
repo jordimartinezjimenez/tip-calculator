dee6f48dcd00f9a4899ea5d7e1ddf266240fa23f
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-04-03 15:30:36 +0200

    Initial commit

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import type { OrderItem } from "../types"
+
+export default function useOrder() {
+
+    const [order, setOrder] = useState<OrderItem[]>([])
+
+    const addItem = () => {
+
+    }
+
+    return {
+        addItem
+    }
+}
