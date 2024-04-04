a3cd457900b49e3a2189cb52003edbb853b3ac58
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-04-04 14:58:44 +0200

    Total and Tip to pay added

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,31 @@
+import { useCallback, useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+  order: OrderItem[],
+  tip: number
+}
+
+export default function OrderTotals({ order, tip }: OrderTotalsProps) {
+
+  const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+  const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
+  // const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order])
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y Propina</h2>
+        <p>Subtotal a pagar: <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+        </p>
+        <p>Propina: <span className="font-bold">{formatCurrency(tipAmount)}</span>
+        </p>
+        <p>Total a pagar: <span className="font-bold">{formatCurrency(totalAmount)}</span>
+        </p>
+      </div>
+      <button></button>
+    </>
+  )
+}
